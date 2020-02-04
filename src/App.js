c9a5a155a05035b4e401514662bce090d16a7cd5
石田航也 <aichibaseball@ishidawatarunari-no-MacBook-Air.local>
2020-02-04 20:01:42 +0900

    implement component using multiple-states

--- src/App.js
+++ src/App.js
@@ -1,49 +1,30 @@
 import React,{useState} from 'react';
 
 
-const App= ()=> {
-  const [count, setCount] = useState(0)
-    //useStateの引数は初期値,
-    //count = state ,setCountはcountの値を操作する関数名
-    //countの値を変更するsetStateの役割
-  const increment = ()=> setCount(count + 1)
-  const decrement = ()=> setCount(count - 1)
-  const increment2 = () =>setCount( previousCount =>
-    previousCount + 1
-  )
-  const decrement2 = () =>setCount( previousCount =>
-    previousCount - 1
-  )
-  const reset = ()=> setCount(0)
-  
-  const double =()=> setCount(count * 2)
-  
-  const divide3 = () =>setCount( previousCount =>
-     previousCount%3 === 0 ? previousCount / 3 : previousCount
-  )
-  
+const App= (props)=> {
+
+  const [name,setName] = useState(props.name)
+  const [price,setPrice] = useState(props.price)
+  const reset = ()=>{
+    setName(props.name)
+    setPrice(props.price)
+  }
+  //resetを呼び出すときに()はいらないのか？
+
   return (
     <React.Fragment>
-      <div>
-      count : {count} 
-      </div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-      </div>
-      <div>
-        <button onClick={double}>×2</button>
-      </div>
-      <div>
-        <button onClick={divide3}>３の倍数の時だけ３で割る</button>
-      </div>
+      <p>{name}は{price}です</p>
+      <button onClick={()=>setPrice(price + 100)}>+100</button>
+      <button onClick={()=>setPrice(price - 100)}>-100</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </React.Fragment>
   );
+}
+
+App.defaultProps = {
+  name: "サンプル",
+  price: 1000
 }
 
 export default App;
