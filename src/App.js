e2d54b894af36eed3f19f12f90b55c1138693701
石田航也 <aichibaseball@ishidawatarunari-no-MacBook-Air.local>
2020-02-04 20:43:48 +0900

    using useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 
 
 const App= (props)=> {
@@ -6,9 +6,21 @@ const App= (props)=> {
   const [state,setState] = useState(props)
   const {name,price} = state
 
+  useEffect(()=>{
+    console.log("nomal useEffect")
+  })
+
+  useEffect(()=>{
+    console.log("didMount")
+  },[])
+
+  useEffect(()=>{
+    console.log("name change")
+  },[name==="takashi"])
+
   return (
     <React.Fragment>
-      <p>{state.name}は{state.price}です</p>
+      <p>{state.name}は{state,price}です</p>
       <button onClick={()=>setState({...state,price: price + 100})}>+100</button>
       <button onClick={()=>setState({...state,price: price - 100})}>-100</button>
       <button onClick={()=>setState(props)}>reset</button>
