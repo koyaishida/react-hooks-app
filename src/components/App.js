b666260d2471599e74ded6aaea034ad2b31f0385
石田航也 <aichibaseball@ishidawatarunari-no-MacBook-Air.local>
2020-02-07 20:54:10 +0900

    setup-react-context-provider

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import reducer from "../reducers";
 import EventForm from "./eventform"
 import Events from "./events"
+import AppContext from "../contexts/AppContext";
 
 const  App = ()=> {
   
@@ -10,10 +11,12 @@ const  App = ()=> {
   //この配列がeventというstate,dispatchがstateを更新する関数
   //この２つをpropsとしてEventFrom,Eventsに渡している
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch}/>
-      <Events state={state} dispatch={dispatch}/>
-    </div>
+    <AppContext.Provider value={"HELLO"}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch}/>
+        <Events state={state} dispatch={dispatch}/>
+      </div>
+    </AppContext.Provider>
   );
 }
 
